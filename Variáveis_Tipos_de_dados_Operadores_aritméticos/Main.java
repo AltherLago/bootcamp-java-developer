167fccb1fb6830be4012d2d64edc0ce0e48f37ae
AltherLago <ajsl@ic.ufal.br>
2022-01-04 20:12:29 -0300

    class 01

---
+++ Variáveis_Tipos_de_dados_Operadores_aritméticos/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int i = 10;
+
+        final int j = 10;
+        //j = 15;
+        int asdfa45asdf;
+        //int asdf fdfd;
+        //int ad$f fdfd;
+
+        int quantidadeProduto = 50;
+        // int qtdProd;
+        //int QUANTIDADE_OPCOES;
+        
+        System.out.println(i);
+    }
+}
